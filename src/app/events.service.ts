9c17f1e24f8460b3cf58beb8b8ce06701a315d73
David <davidharingri@gmail.com>
2021-05-03 11:25:44 +0200

    Worked on events.service

--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ApiService } from './api.service';
+import { AppState } from './store/Store';
+import { Event } from './entities/Event';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventsService {
+export class EventsService extends ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private ngRedux: NgRedux<AppState>) { 
+    super();
+  }
+
+  saveEvent(event: Event) {
+    const token = this.ngRedux.getState().users.token;
+    const url = '';
+  }
 }
